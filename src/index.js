b13ce71653b663ab972a525bde0ce241ff25ee95
hajnalkafenyo <77206669+hajnalkafenyo@users.noreply.github.com>
2025-12-30 18:13:03 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./index.css"
+import { BrowserRouter } from 'react-router-dom';
+import { CartProvider } from './cartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
