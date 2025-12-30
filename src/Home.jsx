57de47c94a599e84edbff8b5b7a7207927ad7a9f
hajnalkafenyo <77206669+hajnalkafenyo@users.noreply.github.com>
2025-12-30 18:18:11 +0100

    add Home

---
+++ src/Home.jsx
@@ -0,0 +1,68 @@
+import { Card } from "./components/card"
+import { useState, useEffect } from 'react'
+
+export function Home() {
+
+    const [products, setProducts] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(undefined)
+
+    useEffect(() => {
+
+        async function fetchData() {
+            setIsLoading(true)
+            try {
+                const res = await fetch("https://v2.api.noroff.dev/online-shop")
+                if (!res.ok) {
+
+                    const data = await res.json()
+
+                    if (data.error) {
+                        setError(data.error.message)
+                        setIsLoading(false)
+                        setProducts([])
+                        return
+                    }
+
+                    setError("API returned invalid state")
+                    setIsLoading(false)
+                    setProducts([])
+                    return
+                }
+                const data = await res.json()
+
+                if (data.error) {
+                    setError(data.error.message)
+                    setIsLoading(false)
+                    setProducts([])
+                    return
+                }
+
+                setProducts(data.data)
+                setIsLoading(false)
+                setError(undefined)
+            } catch (e) {
+                setError(e.message)
+                setIsLoading(false)
+                setProducts([])
+            }
+        }
+
+        fetchData()
+
+    }, [])
+
+    if (isLoading) {
+        return "Loading..."
+    }
+
+    if (error) {
+        return <p>There was an error when loading the data. {error}</p>
+    }
+
+    return <div className="flex flex-row flex-wrap gap-2 justify-center">
+        {products.map((product) => {
+            return <Card id={product.id} title={product.title} description={product.description} rate={product.rating} price={product.price} discountedPrice={product.discountedPrice} image={product.image.url} />
+        })}
+    </div>
+}
---
+++ src/components/card.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export function Card({ id, title, description, image, price, discountedPrice, rate }) {
+    const navigate = useNavigate()
+    return (
+        <div className="border border-gray-300 w-[200px] rounded p-2 flex flex-col bg-white hover:bg-gray-50 hover:border-amber-400 transition-colors cursor-pointer" role="button" tabIndex={0} onClick={() => {
+            navigate(`/product/${id}`)
+        }}>
+            <div className="flex flex-col object-fit">
+                <img src={image} className="h-[160px] w-full object-cover" alt="My awesome Product" />
+            </div>
+
+            <div className=" max-h-8 ">
+                <p className="font-bold font-serif truncate text-gray-800">{title}</p>
+            </div>
+
+            <div className="flex flex-col border-y border-gray-300">
+                <p className="font-sans italic text-sm text-gray-600">{description}</p>
+            </div>
+            <div className="flex flex-row gap-2 justify-between items-end">
+                <div className="text-sm"> {stars(rate, "★")}</div>
+                <div className="flex flex-col">
+                    {discountedPrice !== price ?
+                        <div>
+                            <p className="font-thin line-through text-sm text-gray-500">{price} Kr</p>
+                            <p className="text-amber-600 font-semibold">{discountedPrice} Kr</p>
+                        </div>
+                        :
+                        <p className="text-gray-800 font-semibold">{price} Kr</p>
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export function stars(rate, character) {
+    let b = "";
+    if (rate > 5) {
+        return "★★★★★";
+    }
+    if (rate <= 0) {
+        return "★"
+    }
+    for (let i = 0; i < rate; i++) {
+        b += character
+    }
+    return b
+}
+
