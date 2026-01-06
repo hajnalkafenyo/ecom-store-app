26a2b88681baf2c3713b37fd250069f157732697
hajnalkafenyo <77206669+hajnalkafenyo@users.noreply.github.com>
2026-01-06 17:00:55 +0100

    feat: add home button

--- src/CheckoutSuccess.jsx
+++ src/CheckoutSuccess.jsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom';
 import { OverviewItem } from './components/OverviewItem';
+import { Button } from './components/button';
 import { useCart } from './cartContext';
 import { useState, useEffect } from 'react';
 
@@ -17,6 +19,18 @@ export function CheckoutSuccess() {
     <div className="flex flex-col items-stretch md:items-center gap-2">
       <h1>Thank you for shopping with us!</h1>
       <img src="./checkout.png" alt="Checkout success" />
+      <Link to="/">
+        <Button variant="secondary">
+          Back to home{' '}
+          <img
+            src="/home.svg"
+            height="16px"
+            width="16px"
+            alt="home"
+            className="inline"
+          />{' '}
+        </Button>
+      </Link>
       <div>
         {items.map((item) => {
           return (
